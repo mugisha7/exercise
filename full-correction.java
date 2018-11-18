fa110a2cba579f34e2f18b368196a9b628e85db2
mutabazi <mutabazilionel77@gmail.com>
2018-11-18 04:12:32 -0500

    THIS PROGRAM WAS CORRETED AND ADDED WITH SOME COMMENT

---
+++ full-correction.java
@@ -0,0 +1,7 @@
+public class  exericise{
+	public static void main(String[]jhkjjh){
+	System.out .println("HELLO THERE ");
+	/* this one is correct but the first one was not correct 
+	because the keyword System was not written with first capital leter*/
+	}
+}
